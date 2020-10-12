a4efe4411a4a2d5b60840ac29404bc6e83a942a5
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-10-12 17:01:14 -0300

    renderizando uma lista de Itens utilizando UITableView

--- curso-ios-swift-alura/Controllers/RefeicoesTableViewController.swift
+++ curso-ios-swift-alura/Controllers/RefeicoesTableViewController.swift
@@ -14,4 +14,18 @@ class RefeicoesTableViewController: UITableViewController {
         Refeicao(nome: "Feijoada", felicidade: 5)
     ]
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        
+        celula.textLabel?.text = refeicao.nome
+        print(refeicao.nome)
+        
+        return celula
+    }
+    
 }
